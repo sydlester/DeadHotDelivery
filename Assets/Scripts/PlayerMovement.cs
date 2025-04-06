8930fb1d2d1a207d809d4763554592d4531ae5c5
Phoebe Chadowitz <pc244@duke.edu>
2025-04-06 16:50:06 -0400

    player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    //private Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+    }
+
+    //Moves player in chosen direction
+    private void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+
+    //Triggers walking/idle animations
+    public void Move(InputAction.CallbackContext context)
+    {
+        //animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            //animator.SetBool("isWalking", false);
+            //animator.SetFloat("LastInputX", moveInput.x);
+            //animator.SetFloat("LastInputY", moveInput.y);
+        }
+        moveInput = context.ReadValue<Vector2>();
+        //animator.SetFloat("InputX", moveInput.x);
+        //animator.SetFloat("InputY", moveInput.y);
+        if (moveInput.x != 0)
+        {
+            //spriteRenderer.flipX = moveInput.x < 0;
+        }
+
+    }
+}
