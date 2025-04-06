4a56662935001b4e475a989d8940076dfd32dc2d
sydlester <sydneylester31@gmail.com>
2025-04-06 18:43:24 -0400

    add zombie and basic movement

---
+++ Assets/Scripts/MoveZombie.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveZombie : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float frameCount = 0;
+    public float maxFrameCount = 500;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount++;
+        // check if the zombie is close to the player
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            float distance = Vector2.Distance(rb.position, player.transform.position);
+            if (distance < 5f)
+            // if close enough, move towards the player
+            {
+                MoveTowardsPlayer(player.transform.position);
+            }
+            else
+            // if not close enough, move randomly
+            {
+                MoveRandomly();
+            }
+        }
+        else
+        // if player is not found, move randomly
+        {
+            MoveRandomly();
+        }
+    }
+
+    // return vector to move zombie towards player
+    public void MoveTowardsPlayer(Vector2 playerPosition)
+    {
+        Vector2 direction = (playerPosition - rb.position).normalized;
+        // rb.MovePosition(rb.position + direction * moveSpeed * Time.deltaTime);
+        rb.velocity = direction * moveSpeed / 2;
+    }
+
+    // return vector to move zombie randomly
+    public void MoveRandomly()
+    {
+        if (frameCount >= maxFrameCount)
+        {
+            frameCount = 0;
+            float randomY = Random.Range(-1, 1);
+            float randomX = Random.Range(-1, 1);
+            Vector2 randomDirection = new Vector2(randomX, randomY).normalized;
+            rb.velocity = randomDirection * moveSpeed / 2;
+        }
+    }
+}
