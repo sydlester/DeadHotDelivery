6ba62b2d674483f7c715e8cfd0074a6d607635cb
Phoebe Chadowitz <pc244@duke.edu>
2025-04-06 17:04:40 -0400

    added more sprites

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    //private Animator animator;
+    private Animator animator;
     public SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     //Moves player in chosen direction
@@ -25,20 +26,20 @@ public class PlayerMovement : MonoBehaviour
     //Triggers walking/idle animations
     public void Move(InputAction.CallbackContext context)
     {
-        //animator.SetBool("isWalking", true);
+        animator.SetBool("isWalking", true);
 
         if (context.canceled)
         {
-            //animator.SetBool("isWalking", false);
-            //animator.SetFloat("LastInputX", moveInput.x);
-            //animator.SetFloat("LastInputY", moveInput.y);
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
         }
         moveInput = context.ReadValue<Vector2>();
-        //animator.SetFloat("InputX", moveInput.x);
-        //animator.SetFloat("InputY", moveInput.y);
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
         if (moveInput.x != 0)
         {
-            //spriteRenderer.flipX = moveInput.x < 0;
+            spriteRenderer.flipX = moveInput.x < 0;
         }
 
     }
